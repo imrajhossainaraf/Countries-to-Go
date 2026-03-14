ed7acb6d7a41c5dfb241d212cd63ec47ca03f9dc
Imraj Hossain <imrajhossainaraf12@gmail.com>
2026-03-15 00:58:13 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Countries from "./Components/Countries";
+export default function App() {
+    return (
+        <div>
+            <Countries />
+        </div>
+    );
+}
---
+++ src/Components/Countries.jsx
@@ -0,0 +1,15 @@
+export default function Countries() {
+    return (
+        <div style={{ 
+            textAlign: "center",
+            margin: "20px",
+            padding: "20px",
+            border: "1px solid #ccc",
+            borderRadius: "5px",
+            // backgroundColor: "#f5f5f5",
+         }}>
+            <h2>"In Various Countries"</h2>
+            <p>Here you can find the various countries</p>
+        </div>
+    );
+}
